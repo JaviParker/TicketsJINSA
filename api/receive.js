6a6d5de650ab745292930d0481996ff5f030f8f4
JaviParker <91238693+JaviParker@users.noreply.github.com>
2025-12-29 22:53:01 -0600

    Pusher included
    Pusher included in the code

--- api/receive.js
+++ api/receive.js
@@ -2,10 +2,10 @@
 import Pusher from "pusher";
 
 const pusher = new Pusher({
-  appId: "TU_APP_ID",
-  key: "TU_APP_KEY",
-  secret: "TU_SECRET",
-  cluster: "TU_CLUSTER",
+  appId: "2096563",
+  key: "12326b1d11f72d7bccef",
+  secret: "7dfa573ed136281e4876",
+  cluster: "us2",
   useTLS: true
 });
 
